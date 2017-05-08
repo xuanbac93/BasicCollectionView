d5048d17c30a992389911f293b72bde12c926c44
XuanBac <XuanBac@XuanBacs-Mac.local>
2017-05-09 02:22:27 +0700

    https://github.com/xuanbac93/BasicCollectionView.git

---
+++ BasicCollectionView/MyCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MyCollectionViewCell.swift
+//  BasicCollectionView
+//
+//  Created by XuanBac on 5/9/17.
+//  Copyright © 2017 XuanBac. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var myImageView: UIImageView!
+}
--- BasicCollectionView/ViewController.swift
+++ BasicCollectionView/ViewController.swift
@@ -8,18 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    @IBOutlet weak var MyCollectionView: UICollectionView!
+    
+    var images = ["hinh1", "hinh2", "hinh3", "hinh4", "hinh5"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.MyCollectionView.delegate = self
+        self.MyCollectionView.dataSource = self
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
-}
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collection_cell", for: indexPath) as! MyCollectionViewCell
+        
+        // set images
+        cell.myImageView.image = UIImage(named: images[indexPath.row])
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Selected row is ", indexPath.row)
+    }}
 
